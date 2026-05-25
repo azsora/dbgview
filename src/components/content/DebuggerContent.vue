68bf5442ae170d2a20b19b98ce1b7610d2022c48
azsora <way7539512@gmail.com>
2026-05-25 14:55:51 +0800

    refactor: 更新内容区占位图标为 SVG 串口样式

--- src/components/content/DebuggerContent.vue
+++ src/components/content/DebuggerContent.vue
@@ -14,19 +14,25 @@ const connectionType = computed(() => activeConfig.value.connectionType || 'seri
 
     <!-- 调试器内容区（预留） -->
     <div v-else-if="connectionType === 'debugger'" class="placeholder">
-      <div class="placeholder-icon">🔧</div>
+      <svg class="placeholder-icon" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M20 18c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2H0v2h24v-2h-4zM4 6h16v10H4V6z"/>
+      </svg>
       <div class="placeholder-text">调试器功能开发中...</div>
     </div>
 
     <!-- BLE 内容区（预留） -->
     <div v-else-if="connectionType === 'ble'" class="placeholder">
-      <div class="placeholder-icon">📡</div>
+      <svg class="placeholder-icon" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M17 3H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 14l-4-4h3V9.5l1.5 1.5L10 12H8l3-5 3 5h-2l-1.5-1.5V13h3l-4 4z"/>
+      </svg>
       <div class="placeholder-text">BLE 功能开发中...</div>
     </div>
 
     <!-- TCP/UDP 内容区（预留） -->
     <div v-else-if="connectionType === 'tcp-udp'" class="placeholder">
-      <div class="placeholder-icon">🌐</div>
+      <svg class="placeholder-icon" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
+      </svg>
       <div class="placeholder-text">TCP/UDP 功能开发中...</div>
     </div>
   </div>
--- src/components/content/SerialContent.vue
+++ src/components/content/SerialContent.vue
@@ -111,7 +111,7 @@ function toggleMode() {
       <pre v-if="hasData" v-text="serialStore.state.receiveBuffer"></pre>
       <div v-else class="empty-state">
         <svg class="empty-icon" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 14l-5-5 1.41-1.41L12 14.17l7.59-7.59L21 8l-9 9z"/>
+          <path d="M20 3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-1 16H5V6h14v11zm-3-3h-4v-1h4v1zm-2-3H8V8h6v5z"/>
         </svg>
         <span>无数据</span>
       </div>
